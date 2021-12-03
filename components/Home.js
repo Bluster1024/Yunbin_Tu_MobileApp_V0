35f8ea60376b23b5ea9e337f2cdd70b2176ba6dd
Bluster1024 <60807383+Bluster1024@users.noreply.github.com>
2021-12-03 11:00:33 -0500

    Add files via upload

--- components/Home.js
+++ components/Home.js
@@ -1,72 +1,159 @@
-import React from "react";
-import { View, Text, Button,StyleSheet,TextInput } from "react-native";
+import React , { useState, useEffect }from "react";
+import { View, Text, Button,StyleSheet,TextInput,FlatList,ImageBackground } from "react-native";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
-  const initialList = [
-  {
+const initialList = [
+  { 
+    id : 1,
     name: 'Charlie Card',
     cost: '20',
+    Date: '2021-01-01'
   },
-  {
+  { 
+    id: 2,
     name: 'Drink',
     cost: '15',
+    Date: '2021-01-01'
   },
 ];
+const image = { uri: 'https://images.pexels.com/photos/1366919/pexels-photo-1366919.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500' };
 
 function Home({ navigation }) {
 
 
 const [name, setName] = React.useState('');
+const [cost, setCost] = React.useState('');
 const [list, setList] = React.useState(initialList);
+const [refreshing, setRefreshing] = React.useState(false)
 
-  function handleChange(event) {
-    setName(event.target.value);
+
+  useEffect(() => {readData()},[])
+  
+  const clearAll = async () => {
+        try {
+          console.log('in clearData')
+          await AsyncStorage.clear()
+        } catch(e) {
+          console.log("error in clearData ")
+          console.dir(e)
+          // clear error
+        }
+  }
+
+  const storeData = async()=>{
+      try{
+          const jsonValue = JSON.stringify(list)
+          await AsyncStorage.setItem('@bill', jsonValue)
+      }catch(e){
+        alert('fail to store');
+        console.log(e)
+      }
+  }
+
+  const readData =async() =>{
+    try{
+      const jsonValue = await AsyncStorage.getItem('@bill')
+      let data=null;
+      console.log(jsonValue);
+      if(jsonValue!=null){
+        data=JSON.parse(jsonValue);
+        setList(Array.from(data));
+        console.log(list);
+      }
+    }catch(e) {
+          console.log("error in getData ")
+          console.dir(e)
+          // error reading value
+        }
+  }
+
+  function generateId() {
+      return name+cost+getCurrentDate()+1000*Math.random()
   }
  
   function handleAdd() {
-    const res = name.split(" ");
-    const newList = list.concat({ name:res[0],cost:res[1] });
- 
+    let id=generateId();
+    const newList = list.concat({id:id,name:name,cost:cost,Date:getCurrentDate()});
+
     setList(newList);
+    console.log(newList);
   }
 
+  const getCurrentDate=()=>{
+
+    var date = new Date().getDate();
+    var month = new Date().getMonth() + 1;
+    var year = new Date().getFullYear();
+
+    return year+'-'+month + '-' + date;
+}
+
+const Item = ({ item }) => {
+  return (
+    <View style={{flexDirection:'row'}}>
+      <View style={styles.row}><Text style={styles.text}>{item.name}</Text></View>
+      <View style={styles.row}><Text style={styles.text}>{item.cost}</Text></View>
+      <View style={styles.row}><Text style={styles.text}>{item.Date}</Text></View>
+    </View>
+  );
+}
+
+const renderItem = ({ item }) => (
+    <Item item={item} style={{width: '30%'}}/>
+  );
+
+const handleRefresh = () => {
+  setRefreshing(prevState => !prevState)
+}
+
  return (
-    <View style={{flex:2}}>
-      <View style={{flex:1,alignItems: 'center',justifyContent: 'center'}}>
+    <View >
+      <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+      
+      <View style={{alignItems: 'center',justifyContent: 'center'}}>
         <Text style={styles.header}>Bill statistic</Text>
 
-        <View style={{flexDirection:'row'}}>
-
-          <View style={styles.list}>
-          {list.map((item) => (
-            <li key={item.id}>{item.name}</li>
-          ))}
-          </View>
-
-          <View style={styles.list}>
-          {list.map((item) => (
-            <li key={item.id}>{item.cost}</li>
-          ))}
-          </View>
-
+      
+        <View style={styles.flatList}>
+          <FlatList
+            contentContainerStyle={styles.flatListColumn}
+            data={list}
+            renderItem={renderItem}
+            keyExtractor={item => item.id.toString()}
+            refreshing={refreshing}
+            onRefresh={handleRefresh}
+          />
         </View>
-
       </View>
 
-      <View style={{flex:1}}>
+      <View>
+        <TextInput value={name} placeholder="Type description here  Eg: Lunch" style={styles.textbox} onChangeText={name => setName(name)} />
+        <TextInput value={cost} placeholder="Type cost here  Eg: 15" style={styles.textbox} onChangeText={cost => setCost(cost)} />
+        <Button  title="Add NEW BILL" color='lightblue' onPress={() => handleAdd()}/>
 
-     
 
-        <TextInput value={name} placeholder="Type here  Eg: Lunch 15" style={styles.textbox} onChange={handleChange} />
-        <Button  title="Add NEW BILL" color='lightblue' onPress={() => handleAdd()}>
-        </Button>
+        <Button
+          color='rgb(186,134,255)' title='Save Profile to Memory'
+          onPress = {() => {
+          storeData()
+          }}
+                />
+        <Button
+          color='rgb(118,134,255)' title='Clear memory'
+          onPress = {() => {
+                      console.log('clearing memory');
+                      clearAll()
+                      }}
+        />        
 
         <Button title="Today's Bill" onPress={() => navigation.navigate("Today's Bill")} />
         <Button title="Visualized" color='darkslateblue' onPress={() => navigation.navigate("Visualized")} />
       </View>
-
+      </ImageBackground>
     </View>
  );
+ 
 }
 
 const styles = StyleSheet.create({
@@ -98,10 +185,23 @@ const styles = StyleSheet.create({
   },
   list:{
     margin: 24,
-    alignItems: 'flex.flex-start',
-    justifyContent: 'flex.flex-start',
-    listStyleType: "none"
   },
+  flatList:{
+    justifyContent:'space-between',
+    width:'50%'
+    
+  },
+  flatListColumn:{
+    justifyContent: "space-between"
+  },
+  row:{
+    flex:1,alignItems: 'center',justifyContent: 'center',   backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  text:{
+    fontSize:20,
+    fontWeight:"bold",
+    color:'white'
+  }
 });
 
 export default Home
--- components/Visualized.js
+++ components/Visualized.js
@@ -1,15 +1,37 @@
 import React, { useState } from "react";
-import { View, Text, Button, StyleSheet,TextInput } from "react-native";
+import { View, Text, Button, StyleSheet,TextInput,Image } from "react-native";
+import { Chart, Line, Area, HorizontalAxis, VerticalAxis } from 'react-native-responsive-linechart'
 
 export default function Visualized({ navigation }){
   return(
-    <View style={{flex:1,textAlign: 'center',justifyContent: 'center'}}>
-      <View>
-        <Text>
-          Intend to be a visualized figure of bills
-        </Text>
-        <Button title="Home" onPress={() => navigation.navigate("Home")} />
-      </View>
+    <View style={{flex:1,alignItems:'center',justifyContent: 'center'}}>
+
+<Chart
+  style={{ height: 200, width: 400 }}
+  data={[
+    { x: -2, y: 15 },
+    { x: -1, y: 10 },
+    { x: 0, y: 12 },
+    { x: 1, y: 7 },
+    { x: 2, y: 6 },
+    { x: 3, y: 8 },
+    { x: 4, y: 10 },
+    { x: 5, y: 8 },
+    { x: 6, y: 12 },
+    { x: 7, y: 14 },
+    { x: 8, y: 12 },
+    { x: 9, y: 13.5 },
+    { x: 10, y: 18 },
+  ]}
+  padding={{ left: 40, bottom: 20, right: 20, top: 20 }}
+  xDomain={{ min: -2, max: 10 }}
+  yDomain={{ min: 0, max: 20 }}
+>
+  <VerticalAxis tickCount={11} theme={{ labels: { formatter: (v) => v.toFixed(2) } }} />
+  <HorizontalAxis tickCount={5} />
+  <Area theme={{ gradient: { from: { color: '#ffa502' }, to: { color: '#ffa502', opacity: 0.4 } }}} />
+  <Line theme={{ stroke: { color: '#ffa502', width: 5 }, scatter: { default: { width: 4, height: 4, rx: 2 }} }} />
+</Chart>
 
       </View>
   )
@@ -37,5 +59,9 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+    tinyLogo: {
+    width: 50,
+    height: 50,
   },
 });
